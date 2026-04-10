c551abb623924f61d24a1ad5e8ed2e4d672d7357
3manuel0 <saiddimension@gmail.com>
2026-04-10 01:11:57 +0100

    test changed a bit

--- game.c
+++ game.c
@@ -1,10 +1,13 @@
-#include"raylib.h"
+#include"include/raylib.h"
 
 #define screenWidth 1280
 #define screenHeight 720
 
 static Rectangle rect = {50, 50, 200, 200};
+static Rectangle rect2 = {200, 100, 200, 200};
 static Vector2 velocity = {200, 200};
+static Vector2 velocity2 = {150, 150};
+
 
 
 // perfect velocity
@@ -18,19 +21,26 @@ void GameInit(){
 
 void GameFrame(){
     BeginDrawing();
-    ClearBackground(RAYWHITE);
+    ClearBackground((Color){0xcd, 0xd6, 0xf4, 0xff});
     float dt = GetFrameTime();
-    float x =  rect.x + velocity.x*dt;
     if((rect.x < 0 && velocity.x < 0)|| (rect.x  + rect.width >= GetScreenWidth() && velocity.x > 0 )){
         velocity.x *= -1;
     }
-    rect.x = x;
-    float y =  rect.y + velocity.y*dt;
     if((rect.y < 0 && velocity.y < 0) || (rect.y  + rect.height >= GetScreenHeight() && velocity.y > 0)){
         velocity.y *= -1;
     }
-    rect.y  = y;
-    // DrawRectangleRec(rect, BLACK);
+    rect.y += velocity.y*dt;
+    rect.x += velocity.x*dt;
+    if((rect2.x < 0 && velocity2.x < 0)|| (rect2.x  + rect2.width >= GetScreenWidth() && velocity2.x > 0 )){
+        velocity2.x *= -1;
+    }
+    if((rect2.y < 0 && velocity2.y < 0) || (rect2.y  + rect2.height >= GetScreenHeight() && velocity2.y > 0)){
+        velocity2.y *= -1;
+    }
+    rect2.y += velocity2.y*dt;
+    rect2.x += velocity2.x*dt;
+    DrawRectangleRec(rect2, ORANGE);
+    DrawText("This is just a test for this library", 0, 0, 24, BLACK);
     DrawFPS(screenWidth - 90 , 0);
     DrawRectangle(rect.x, rect.y, rect.width, rect.height, DARKBLUE);
     EndDrawing();
